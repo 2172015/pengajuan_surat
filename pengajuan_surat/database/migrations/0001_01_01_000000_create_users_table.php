397d32e31884f09b62aebf708078bb380949d4a3
2172015 <2172015@maranatha.ac.id>
2025-03-18 18:11:16 +0700

    18-3-25 18:11

--- pengajuan_surat/database/migrations/0001_01_01_000000_create_users_table.php
+++ pengajuan_surat/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('nama');
+            $table->string('email')->unique();
+            $table->string('no_telefon');
+            $table->timestamp('email_verified_at')->nullable();
+            $table -> enum('role',['admin', 'mahasiswa'])
+            ->default('mahasiswa');
             $table->rememberToken();
             $table->timestamps();
         });
